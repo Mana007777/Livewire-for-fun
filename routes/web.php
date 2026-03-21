2ba8af88e4fd98791faf37ee4d16445e70655177
mana007777 <e7ifubbb@gmail.com>
2026-03-21 19:18:43 +0300

    Rest of the routes route

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,36 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::livewire('dashboard', 'admin.dashboard')->name('dashboard');
+    Route::livewire('d  ashboard', 'admin.dashboard')->name('dashboard');
     Route::prefix('companies')->group(function () {
         Route::livewire('/', 'admin.companies.index')->name('companies.index');
         Route::livewire('/create', 'admin.companies.create')->name('companies.create');
-        Route::livewire('/{company}/edit', 'admin.companies.edit')->name('companies.edit');
+        Route::livewire('/{id}/edit', 'admin.companies.edit')->name('companies.edit');
     });
-})->name('dashboard');
+    Route::prefix('departments')->group(function () {
+        Route::livewire('/', 'admin.departments.index')->name('departments.index');
+        Route::livewire('/create', 'admin.departments.create')->name('departments.create');
+        Route::livewire('/{id}/edit', 'admin.departments.edit')->name('departments.edit');
+    });
+    Route::prefix('designations')->group(function () {
+        Route::livewire('/', 'admin.designations.index')->name('designations.index');
+        Route::livewire('/create', 'admin.designations.create')->name('designations.create');
+        Route::livewire('/{id}/edit', 'admin.designations.edit')->name('designations.edit');
+    });
+    Route::prefix('employees')->group(function () {
+        Route::livewire('/', 'admin.employees.index')->name('employees.index');
+        Route::livewire('/create', 'admin.employees.create')->name('employees.create');
+        Route::livewire('/{id}/edit', 'admin.employees.edit')->name('employees.edit');
+    });
+    Route::prefix('contracts')->group(function () {
+        Route::livewire('/', 'admin.contracts.index')->name('contracts.index');
+        Route::livewire('/create', 'admin.contracts.create')->name('contracts.create');
+        Route::livewire('/{id}/edit', 'admin.contracts.edit')->name('contracts.edit');
+    });
+    Route::prefix('payroll')->group(function () {
+        Route::livewire('/', 'admin.payroll.index')->name('payroll.index');
+        Route::livewire('/{id}/show', 'admin.payroll.show')->name('payroll.show');
+    });
+});
 
 require __DIR__.'/settings.php';
