dd1c76d71fb829c1a9cab1a72071915949e667a4
mana007777 <e7ifubbb@gmail.com>
2026-03-21 19:07:57 +0300

    companies route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::livewire('dashboard', 'admin.dashboard')->name('dashboard');
-});
+    Route::prefix('companies')->group(function () {
+        Route::livewire('/', 'admin.companies.index')->name('companies.index');
+        Route::livewire('/create', 'admin.companies.create')->name('companies.create');
+        Route::livewire('/{company}/edit', 'admin.companies.edit')->name('companies.edit');
+    });
+})->name('dashboard');
 
 require __DIR__.'/settings.php';
