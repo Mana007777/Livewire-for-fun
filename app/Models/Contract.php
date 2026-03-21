3cb42b0aa3a11667770753f2e1f4d380cc72bfbd
mana007777 <e7ifubbb@gmail.com>
2026-03-21 09:13:57 +0300

    Contract model

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -2,9 +2,44 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Contract extends Model
 {
-    //
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+    
+    public function designation()
+    {
+        return $this->belongsTo(Designation::class);
+    }
+
+    public function scopeInCompany($query)
+    {
+        return $query->whereHas('employee', function ($q) {
+            $q->isCompany();
+        });
+    }
+
+    public function getDurationAttribute()
+    {
+        return Carbon::parse($this->start_date)->diffInDays(Carbon::parse($this->end_date));
+    }
+
+    public function scopeSearch($query, $term)
+    {
+        return $query->whereHas('employee', function ($q) use ($term) {
+            $q->where('name', 'LIKE', "%$term%");
+        });
+    }
+
+    public function getTotalEarnings($monthYear)
+    {
+        return $this->rate_type === 'monthly' ? $this->rate : $this->rate * Carbon::parse($monthYear)->daysInMonth;
+    }
+
+    
 }
