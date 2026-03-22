a551ae179fb73f46c925afa02340d6e170587f33
mana007777 <e7ifubbb@gmail.com>
2026-03-22 09:01:13 +0300

    dashboard route fixed

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::livewire('d  ashboard', 'admin.dashboard')->name('dashboard');
+    Route::livewire('dashboard', 'admin.dashboard')->name('dashboard');
     Route::prefix('companies')->group(function () {
         Route::livewire('/', 'admin.companies.index')->name('companies.index');
         Route::livewire('/create', 'admin.companies.create')->name('companies.create');
