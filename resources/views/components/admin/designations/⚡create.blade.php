aee30f6e050f382a450a07d4f9d24207de48df90
mana007777 <e7ifubbb@gmail.com>
2026-03-23 11:27:37 +0300

    Designation create

--- resources/views/components/admin/designations/⚡create.blade.php
+++ resources/views/components/admin/designations/⚡create.blade.php
@@ -1,10 +1,28 @@
 <?php
 
 use Livewire\Component;
-
+use App\Models\Designation;
 new class extends Component
 {
-    //
+    public $designation;
+    public function rules()
+    {
+        return [
+            'designation.name' => 'required|string|max:255|unique:designations',
+            'designation.department_id' => 'required|exists:departments,id',
+            ];
+    }
+    public function mount()
+    {
+        $this->designation = new Designation();
+    }
+    public function save()
+    {
+        $this->validate();
+        $this->designation->save();
+        session()->flash('success', 'Designation created successfully.');
+        return $this->redirectIntended('designations.index');
+    }
 };
 ?>
 
