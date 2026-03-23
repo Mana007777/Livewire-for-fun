c2e36a8856c5b3078e8deb8366fe7234915de6fe
mana007777 <e7ifubbb@gmail.com>
2026-03-23 11:18:20 +0300

    Edit contract

--- resources/views/components/admin/contracts/⚡edit.blade.php
+++ resources/views/components/admin/contracts/⚡edit.blade.php
@@ -1,10 +1,30 @@
 <?php
 
+use App\Models\Department;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public $department;
+    public function rules()
+    {
+        return [
+            'department' => 'required|string|max:255',
+        ];
+    }
+
+    public function mount($id)
+    {
+        $this->department = Department::find($id);
+    }   
+
+    public function save()
+    {
+        $this->validate();
+        $this->department->save();
+        session()->flash('success', 'Department updated successfully.');
+        return $this->redirectIntended(route('departments.index'));
+    }
 };
 ?>
 
