cedaac08ec144667cdb63291bf0e882f4e3601c0
mana007777 <e7ifubbb@gmail.com>
2026-03-23 11:08:46 +0300

    Edit companies

--- resources/views/components/admin/contracts/⚡index.blade.php
+++ resources/views/components/admin/contracts/⚡index.blade.php
@@ -1,10 +1,23 @@
 <?php
 
+use App\Models\Department;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 new class extends Component
 {
-    //
+    use WithPagination  , WithoutUrlPagination;
+
+    public function delete($id)
+    {
+        Department::find($id)->delete();
+        session()->flash('success', 'Department deleted successfully.');
+    }
+    public function getCompaniesProperty()
+    {
+        return Department::latest()->paginate(10);
+    }
 };
 ?>
 
