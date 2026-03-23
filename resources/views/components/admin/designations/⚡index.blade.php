77eaa7ad34215140061f76f6049fe5bf6fb9ff71
mana007777 <e7ifubbb@gmail.com>
2026-03-23 11:24:13 +0300

    Designation index

--- resources/views/components/admin/designations/⚡index.blade.php
+++ resources/views/components/admin/designations/⚡index.blade.php
@@ -1,10 +1,23 @@
 <?php
 
+use App\Models\Designation;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 new class extends Component
 {
-    //
+    use WithPagination  , WithoutUrlPagination;
+
+    public function delete($id)
+    {
+        Designation::find($id)->delete();
+        session()->flash('success', 'Designation deleted successfully.');
+    }
+    public function getCompaniesProperty()
+    {
+        return Designation::inCompany()->paginate(5);
+    }
 };
 ?>
 
