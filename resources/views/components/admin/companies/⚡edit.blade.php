52c1ac27d34c37019ff2a44fe70d3bc383a7b29a
mana007777 <e7ifubbb@gmail.com>
2026-03-23 10:59:02 +0300

    Edit companies

--- resources/views/components/admin/companies/⚡edit.blade.php
+++ resources/views/components/admin/companies/⚡edit.blade.php
@@ -1,10 +1,35 @@
 <?php
 
+use App\Models\Company;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 new class extends Component
 {
-    //
+     use WithFileUploads;
+    public $company;
+
+    public $logo;
+    public function rules(){
+        return [
+            'company.name' => 'required|string|max:255',
+            'company.email' => 'required|email|max:255',
+            'company.website' => 'nullable|url|max:255',
+            'logo' => 'nullable|image|max:2048|mimes:jpeg,png,jpg,svg', // 1MB Max
+        ];
+    }
+    public function mount($id){
+        $this->company =Company::find($id);
+    }
+    public function save(){
+        $this->validate();
+        if($this->logo){
+            $this->company->logo = $this->logo->store('logos', 'public');
+        }
+        $this->company->save();
+        session()->flash('success', 'Company created successfully.');
+        return $this->redirectIntended(route('companies.index'));
+    }
 };
 ?>
 
