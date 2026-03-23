8103bf0dc2861c648c26952a95408534a1f7e11b
mana007777 <e7ifubbb@gmail.com>
2026-03-23 12:39:58 +0300

    contract create

--- resources/views/components/admin/contracts/⚡create.blade.php
+++ resources/views/components/admin/contracts/⚡create.blade.php
@@ -1,11 +1,62 @@
 <?php
 
+use App\Models\Contract;
 use App\Models\Department;
+use App\Models\Employee;
 use Livewire\Component;
 
 new class extends Component
 {
-    
+    public $contract;
+
+    public $search = '';
+
+    public $department_id;
+
+    public function rules()
+    {
+        return [
+            'contract.designation_id' => 'required',
+            'contract.employee_id' => 'nullable|exists:employees,id',
+            'contract.start_date' => 'required|date',
+            'contract.end_date' => 'required|date|after_or_equal:contract.start_date',
+            'contract.rate_type' => 'required',
+            'contract.rate' => 'required|numeric',
+        ];
+    }
+    public function mount()
+    {
+        $this->contract = new Contract();
+    }
+
+    public function selectEmployee($id)
+    {
+        $this->contract->employee_id = $id;
+        $this->search = $this->contract->employee->name;
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->contract->save();
+
+        session()->flash('message', 'Contract created successfully.');
+
+        return $this->redirectIntended(route('contracts.index'));
+    }
+    public function searchEmployees()
+    {
+        $employees = Employee::inCompany()->scopeSearch($this->search)->get();
+    }
+    public function departments()
+    {
+        return Department::inCompany()->get();  
+    }
+    public function designations()
+    {
+        return $this->department_id ? Department::find($this->department_id)->designations : collect();
+        } 
 };
 ?>
 
