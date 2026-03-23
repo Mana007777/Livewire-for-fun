d9165fd43d864a5812d8c76a3ec9cb093982ecf9
mana007777 <e7ifubbb@gmail.com>
2026-03-23 11:16:41 +0300

    create contract

--- resources/views/components/admin/contracts/⚡create.blade.php
+++ resources/views/components/admin/contracts/⚡create.blade.php
@@ -1,10 +1,30 @@
 <?php
 
+use App\Models\Department;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public $department;
+    public function rules()
+    {
+        return [
+            'department' => 'required|string|max:255',
+        ];
+    }
+
+    public function mount()
+    {
+        $this->department = new Department();
+    }
+
+    public function save()
+    {
+        $this->validate();
+        $this->department->save();
+        session()->flash('success', 'Department created successfully.');
+        return $this->redirectIntended(route('departments.index'));
+    }
 };
 ?>
 
