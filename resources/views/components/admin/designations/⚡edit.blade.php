5989423ad52ed3f436bf53d4d0f22405bb7c4c7b
mana007777 <e7ifubbb@gmail.com>
2026-03-23 11:29:47 +0300

    Designation edit

--- resources/views/components/admin/designations/⚡edit.blade.php
+++ resources/views/components/admin/designations/⚡edit.blade.php
@@ -1,10 +1,29 @@
 <?php
 
+use App\Models\Designation;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public $designation;
+    public function rules()
+    {
+        return [
+            'designation.name' => 'required|string|max:255|unique:designations',
+            'designation.department_id' => 'required|exists:departments,id',
+            ];
+    }
+    public function mount($id)
+    {
+        $this->designation =Designation::find($id);
+    }
+    public function save()
+    {
+        $this->validate();
+        $this->designation->save();
+        session()->flash('success', 'Designation updated successfully.');
+        return $this->redirectIntended('designations.index');
+    }
 };
 ?>
 
